663dc55afdcbd267a8a2a209f37e5103bfd04990
Florent Biville <fbiville@pivotal.io>
2019-08-08 12:16:46 +0200

    Initial implementation

---
+++ README.md
@@ -0,0 +1,14 @@
+# Understanding relation between `Writable#end` and `finish` event
+
+## Install deps
+```sh
+ $ yarn
+```
+
+## Observe the problem
+```sh
+ $ yarn test
+```
+
+Understand how and why this happens?
+Please explain [here](https://github.com/nodejs/help/issues/2112).
---
+++ spec/finish-not-fired.spec.js
@@ -0,0 +1,95 @@
+const {Writable, PassThrough, Transform} = require('stream');
+
+
+describe("streaming pipeline =>", () => {
+
+    let source;
+    let pipeline;
+    let destination;
+
+    beforeEach(() => {
+        const options = {objectMode: true};
+        source = new PassThrough(options);
+        destination = new PassThrough(options);
+        pipeline = new Pipeline(options, destination);
+    });
+
+    afterEach(() => {
+        source.destroy();
+        pipeline.destroy();
+        destination.destroy();
+    });
+
+    it("should end if one of the input streams errors =>", (done) => {
+        let errored = false;
+        pipeline.on("error", (err) => {
+            expect(err.type).toEqual('invalid.payload');
+            expect(err.cause).toEqual('cannot handle: nope');
+            errored = true;
+        });
+
+        pipeline.on("finish", () => {
+            expect(errored).toBeTruthy();
+            done();
+        });
+        source.pipe(pipeline);
+        source.write({"index": 1, "data": "nope"});
+    })
+});
+
+
+class Pipeline extends Writable {
+    constructor(options, destinationStream) {
+        super(options);
+        this.destinationStream = destinationStream;
+        this.inputStreams = [
+            new InputTransform(options),
+            new InputTransform(options),
+            new InputTransform(options)
+        ];
+        this.inputStreams.forEach((it) => {
+            it.on('error', (err) => {
+                this.emit('error', err);
+            });
+            it.pipe(this.destinationStream);
+        });
+        this.on('finish', () => {
+            this.inputStreams.forEach((it) => it.emit('end'));
+        });
+        this.on('error', () => {
+            this.end();
+        })
+    }
+
+    _write(chunk, _, callback) {
+        this.inputStreams[chunk['index']].write(chunk['data']);
+    }
+}
+
+class InputTransform extends Transform {
+    constructor(options) {
+        super(options);
+    }
+
+    _transform(chunk, _, callback) {
+        if (chunk === "nope") {
+            callback(new MyCustomError('invalid.payload', `cannot handle: ${chunk}`))
+        } else {
+            callback(null, chunk);
+        }
+    }
+}
+
+class MyCustomError extends Error {
+    constructor(type, cause) {
+        super();
+
+        // Maintains proper stack trace for where our error was thrown (only available on V8)
+        if (Error.captureStackTrace) {
+            Error.captureStackTrace(this, MyCustomError);
+        }
+
+        this.type = type;
+        this.cause = cause;
+    }
+}
